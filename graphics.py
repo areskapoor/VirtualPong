879f8a7db78c9363a048cec389a1d9527c300fd2
JY-Leee <96390043+JY-Leee@users.noreply.github.com>
2022-04-03 01:46:43 -0400

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -3,42 +3,70 @@ from time import *
 def drawpongTable():
     pongTable=box(pos=vector(0,0,0),color=color.white,length=8, width=20, height=.8)
 
+
 def drawBall():
     ball=sphere (color=color.orange, radius=.4)
     ddy=-0.00098
     dy=0
-    dz=-0.2
+    dz=-0.15
     xPos=0
     yPos=7
     zPos=10
+    loc=cupLocations()
     while True:
         rate(20)
         dy=dy+ddy
         yPos=yPos+dy
         zPos=zPos+dz
         ball.pos=vector(xPos,yPos,zPos)
+        if ball.pos[1]<1.5:
+            return 
+        print(ball.pos)
+        
+        
 
 def drawCupHelper(x,z):
     #https://www.glowscript.org/#/user/GlowScriptDemos/folder/Examples/program/Extrusions/edit
     tube = extrusion(path=[vec(0,0,0), vec(1.5,0,0)], shape=shapes.circle(radius=0.6, thickness=0.1),
                     pos=vec(x,1,-z), axis=vec(0,2,0), color=color.red, end_face_color=color.white)
 
+def cupLocations():
+    cup1Loc=[-0.6,1,9.4]
+    cup2Loc=[0.6,1,9.4]
+    cup3Loc=[-1.8,1,9.4]
+    cup4Loc=[1.8,1,9.4]
+    cup5Loc=[0,1,8.2]
+    cup6Loc=[-1.2,1,8.2]
+    cup7Loc=[1.2,1,8.2]
+    cup8Loc=[-0.6,1,7]
+    cup9Loc=[0.6,1,7]
+    cup10Loc=[0,1,5.8]
+    cupLocations=[cup1Loc,cup2Loc,cup3Loc,cup4Loc,
+        cup5Loc,cup6Loc,cup7Loc,cup8Loc
+        ,cup9Loc,cup10Loc]
+
+    
+    return cupLocations
+
+
 def drawCup():
-    cup1= drawCupHelper(-0.6,9.4)
-    cup2= drawCupHelper(0.6,9.4)
-    cup3= drawCupHelper(-1.8,9.4)
-    cup4= drawCupHelper(1.8,9.4)
-    cup5= drawCupHelper(0,8.2)
-    cup6= drawCupHelper(-1.2,8.2)
-    cup7= drawCupHelper(1.2,8.2)
-    cup8= drawCupHelper(-0.6,7)
-    cup9= drawCupHelper(0.6,7)
-    cup10= drawCupHelper(0,5.8) 
+    loc=cupLocations()
+    cup1= drawCupHelper(loc[0][0],loc[0][2])
+    cup2= drawCupHelper(loc[1][0],loc[1][2])
+    cup3= drawCupHelper(loc[2][0],loc[2][2])
+    cup4= drawCupHelper(loc[3][0],loc[3][2])
+    cup5= drawCupHelper(loc[4][0],loc[4][2])
+    cup6= drawCupHelper(loc[5][0],loc[5][2])
+    cup7= drawCupHelper(loc[6][0],loc[6][2])
+    cup8= drawCupHelper(loc[7][0],loc[7][2])
+    cup9= drawCupHelper(loc[8][0],loc[8][2])
+    cup10= drawCupHelper(loc[9][0],loc[9][2])
 
 def virtualPong():
     drawCup()
     drawpongTable()
     drawBall()
+    
 
 
 scene.camera.pos= vector(0, 6.52134, 10)
